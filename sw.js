c7dadcd1207605ce16a52b0cb229afb757414165
Khatrao84 <133596110+Khatrao84@users.noreply.github.com>
2026-03-17 20:11:28 +0530

    Update CACHE_NAME to version 30.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gurbani-v30.4'; 
+const CACHE_NAME = 'gurbani-v30.5'; 
 const ASSETS = ['/', '/index.html', '/manifest.json', '/favicon.png', '/haarimandr.png', '/audioharimandar.png'];
 
 self.addEventListener('install', (e) => {
