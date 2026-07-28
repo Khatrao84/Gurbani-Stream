b366e4c386f1e267c5070cafd9f93697dd434684
Khatrao84 <133596110+Khatrao84@users.noreply.github.com>
2026-07-28 20:39:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gurbani-v31.3'; 
+const CACHE_NAME = 'gurbani-v31.4'; 
 const ASSETS = ['/', '/index.html', '/manifest.json', '/favicon.png', '/haarimandr.png', '/audioharimandar.png'];
 
 self.addEventListener('install', (e) => {
